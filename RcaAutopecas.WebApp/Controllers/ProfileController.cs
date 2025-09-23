8d7d34969ca82a68c9fb7cf897b667765337211b
Felipe Ramires <felipe.ramires482@gmail.com>
2025-09-23 16:47:58 -0300

    Ajusta código

--- RcaAutopecas.WebApp/Controllers/ProfileController.cs
+++ RcaAutopecas.WebApp/Controllers/ProfileController.cs
@@ -1,32 +1,28 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity; 
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using RcaAutopecas.WebApp.Models; 
-using System.Threading.Tasks; 
+using RcaAutopecas.WebApp.Models;
+using System.Threading.Tasks;
 
 namespace RcaAutopecas.WebApp.Controllers
 {
     [Authorize]
-    public class HomeController : Controller
+    public class ProfileController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
-
-        // Injeta o UserManager para poder buscar os dados do usuário
-        public HomeController(UserManager<ApplicationUser> userManager)
+        public ProfileController(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
         }
-
+              
         public async Task<IActionResult> Index()
         {
-            // Busca o usuário completo logado
             var user = await _userManager.GetUserAsync(User);
 
-            // Passa o nome da empresa para a View
             ViewData["NomeDaEmpresa"] = user?.NomeDaEmpresa;
-
+            
             return View();
         }
-       
+
     }
 }
