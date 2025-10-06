27c266d56345fecedd2e8b5b0c1a96d6ada5d39c
Felipe Ramires <felipe.ramires482@gmail.com>
2025-10-06 16:28:08 -0300

    feat(ui): Adicona tooltip nos icone do menu lateral

--- RcaAutopecas.WebApp/wwwroot/js/site.js
+++ RcaAutopecas.WebApp/wwwroot/js/site.js
@@ -7,3 +7,6 @@
         }, 5000);
     }
 });
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
